388ac9acbb0788848d19497328f4f09de9e4caea
bux578 <github@jonathandavid.de>
2016-06-20 19:05:13 +0200

    ACEX logo, close #14 (#21)
    * add ACEX logo
    
    * change mod logo
    
    * Use new logo in make.py as well

--- mod.cpp
+++ mod.cpp
@@ -1,12 +1,12 @@
 name = "Advanced Combat Environment Extras 3.0.0";
-picture = "logo_ace3_ca.paa";
+picture = "logo_acex_ca.paa";
 actionName = "GitHub";
 action = "https://github.com/acemod/ACEX";
 description = "ACEX - Version 3.0.0";
-logo = "logo_ace3_ca.paa";
-logoOver = "logo_ace3_ca.paa";
+logo = "logo_acex_ca.paa";
+logoOver = "logo_acex_ca.paa";
 tooltip = "ACEX";
 tooltipOwned = "ACEX Owned";
 overview = "Advanced Combat Environment Extras, also known as ACEX, adds a variety of additional systems on top of the ACE3 framework.";
 author = "ACE3 Team";
-overviewPicture = "logo_ace3_ca.paa";
+overviewPicture = "logo_acex_ca.paa";
