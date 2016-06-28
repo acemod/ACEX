62254c4c367848f93f7c570df4a769a3bd15cfee
Glowbal <thomasskooi@live.nl>
2016-06-28 22:52:58 +0200

    fix versioning config error

--- addons/main/script_mod.hpp
+++ addons/main/script_mod.hpp
@@ -15,4 +15,4 @@
 
 // MINIMAL required version for the Mod. Components can specify others..
 #define REQUIRED_VERSION 1.60
-#define REQUIRED_ACE_VERSION {3.6.0}
+#define REQUIRED_ACE_VERSION {3,6,0}
