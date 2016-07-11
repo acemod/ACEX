9fcb2be1ed1ebcee9f7ecf0c8deeedbc10d86fb6
James Kingsley <jameslkingsley@outlook.com>
2016-07-12 00:07:46 +0100

    Add default distance macro, disable debug mode

--- addons/quickmount/script_component.hpp
+++ addons/quickmount/script_component.hpp
@@ -1,7 +1,7 @@
 #define COMPONENT quickmount
 #include "\z\acex\addons\main\script_mod.hpp"
 
-#define DEBUG_MODE_FULL
+// #define DEBUG_MODE_FULL
 // #define DISABLE_COMPILE_CACHE
 // #define CBA_DEBUG_SYNCHRONOUS
 // #define ENABLE_PERFORMANCE_COUNTERS
@@ -15,3 +15,5 @@
 #endif
 
 #include "\z\acex\addons\main\script_macros.hpp"
+
+#define DEFAULT_DISTANCE 3
