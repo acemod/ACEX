633faead473b7e55e1f8d91403b3ede1695dc353
James Kingsley <jameslkingsley@outlook.com>
2016-07-11 23:14:04 +0100

    Add ACE settings

---
+++ addons/quickmount/ACE_Settings.hpp
@@ -0,0 +1,21 @@
+class ACE_Settings {
+    class GVAR(Enabled) {
+        value = 1;
+        typeName = "BOOL";
+        category = CSTRING(Category);
+        displayName = ACECSTRING(common,Enabled);
+        description = CSTRING(KeybindDescription);
+        isClientSettable = 0;
+        force = 1;
+    };
+    class GVAR(distance) {
+        value = 3;
+        typeName = "SCALAR";
+        category = CSTRING(Category);
+        displayName = ACECSTRING(common,Enabled);
+        description = CSTRING(KeybindDescription);
+        isClientSettable = 0;
+        force = 1;
+        values[] = {"0m", "1m", "2m", "3m", "4m", "5m", "6m", "7m", "8m", "9m", "10m"};
+    };
+};
--- addons/quickmount/XEH_preInit.sqf
+++ addons/quickmount/XEH_preInit.sqf
@@ -4,6 +4,4 @@ ADDON = false;
 
 #include "XEH_PREP.hpp"
 
-GVAR(distance) = 2.5;
-
 ADDON = true;
