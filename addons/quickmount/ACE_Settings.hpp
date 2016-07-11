4b25d7cb75461c52fd29838314f704b6302a4903
James Kingsley <jameslkingsley@outlook.com>
2016-07-12 00:08:28 +0100

    Update value, remove values property

--- addons/quickmount/ACE_Settings.hpp
+++ addons/quickmount/ACE_Settings.hpp
@@ -9,13 +9,12 @@ class ACE_Settings {
         force = 0;
     };
     class GVAR(distance) {
-        value = 3;
+        value = DEFAULT_DISTANCE;
         typeName = "SCALAR";
         category = CSTRING(Category);
         displayName = ACECSTRING(common,Enabled);
         description = CSTRING(KeybindDescription);
         isClientSettable = 0;
         force = 0;
-        values[] = {"0m", "1m", "2m", "3m", "4m", "5m", "6m", "7m", "8m", "9m", "10m"};
     };
 };
