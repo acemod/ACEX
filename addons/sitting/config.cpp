ff7ea92b4826e2243e41b7017387272f99a00414
PabstMirror <pabstmirror@gmail.com>
2016-10-19 12:50:38 -0500

    ACE_sitting inherit version from ace_common (#41)
    * ACE_sitting inherit version from ace_common
    
    * New line

--- addons/sitting/config.cpp
+++ addons/sitting/config.cpp
@@ -12,6 +12,20 @@ class CfgPatches {
         url = ACECSTRING(main,URL);
         VERSION_CONFIG;
     };
+
+    // backwards compatability
+    class ACE_common;
+    class ACE_Sitting: ACE_common {
+        name = COMPONENT_NAME;
+        units[] = {};
+        weapons[] = {};
+        requiredVersion = REQUIRED_VERSION;
+        requiredAddons[] = {};
+        author = ACECSTRING(common,ACETeam);
+        authors[] = {"Jonpas"};
+        url = ACECSTRING(main,URL);
+        // VERSION_CONFIG will be inherited from ace for version checking
+    };
 };
 
 #include "ACE_Settings.hpp"
