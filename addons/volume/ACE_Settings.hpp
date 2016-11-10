808d8c200eca74a52db401ecc08c7ce794bcf7b9
James Kingsley <jameslkingsley@outlook.com>
2016-11-10 15:16:14 +0000

    Add fade delay ACE setting

--- addons/volume/ACE_Settings.hpp
+++ addons/volume/ACE_Settings.hpp
@@ -18,4 +18,14 @@ class ACE_Settings {
         force = 0;
         values[] = {"0%", "10%", "20%", "30%", "40%", "50%", "60%", "70%", "80%", "90%", "100%"};
     };
+    class GVAR(fadeDelay) {
+        value = 1;
+        typeName = "SCALAR";
+        category = CSTRING(Name);
+        displayName = CSTRING(FadeDelay);
+        description = CSTRING(FadeDelayDescription);
+        isClientSettable = 1;
+        force = 0;
+        values[] = {"0s", "1s", "2s", "3s", "4s", "5s", "6s", "7s", "8s", "9s", "10s"};
+    };
 };
--- addons/volume/XEH_preInit.sqf
+++ addons/volume/XEH_preInit.sqf
@@ -5,7 +5,6 @@ ADDON = false;
 #include "XEH_PREP.hpp"
 
 GVAR(isLowered) = false;
-GVAR(fadeDelay) = 1;
 GVAR(initialGameVolume) = soundVolume;
 GVAR(initialMusicVolume) = musicVolume;
 
