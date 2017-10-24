38b122097d62120ee8e02bb44383f3298fddd341
PabstMirror <pabstmirror@gmail.com>
2017-10-24 13:03:07 -0500

    Update required ACE version (showMouseHint)

--- addons/main/script_mod.hpp
+++ addons/main/script_mod.hpp
@@ -9,8 +9,8 @@
 #define VERSION_AR MAJOR,MINOR,PATCHLVL,BUILD
 
 // MINIMAL required version for the Mod. Components can specify others..
-#define REQUIRED_VERSION 1.64
-#define REQUIRED_ACE_VERSION {3,9,0}
+#define REQUIRED_VERSION 1.76
+#define REQUIRED_ACE_VERSION {3,11,0}
 
 #ifdef COMPONENT_BEAUTIFIED
     #define COMPONENT_NAME QUOTE(ACEX - COMPONENT_BEAUTIFIED)
