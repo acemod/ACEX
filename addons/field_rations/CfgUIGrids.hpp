9500715a5f29e2a701abac57a901e78e40f310b8
Unknown <mhariszakar@gmail.com>
2018-08-07 17:32:12 -0400

    Re-add repositioning of HUD and update IGUI preview

--- addons/field_rations/CfgUIGrids.hpp
+++ addons/field_rations/CfgUIGrids.hpp
@@ -3,13 +3,22 @@ class CfgUIGrids {
         class Presets {
             class Arma3 {
                 class Variables {
-                    grid_ACEX_FieldRations[] = {{"(safeZoneX + safeZoneW) - (4.2 * ((safeZoneW / safeZoneH) min 1.2) / 40)", "(safeZoneY + safeZoneH) - (2.1 * (((safeZoneW / safeZoneH) min 1.2) / 1.2) / 25)", "4.1 * (((safeZoneW / safeZoneH) min 1.2) / 40)", "2 * ((((safeZoneW / safeZoneH) min 1.2) / 1.2) / 25)"}, "(((safeZoneW / safeZoneH) min 1.2) / 40)", "((((safeZoneW / safeZoneH) min 1.2) / 1.2) / 25)"};
+                    GVAR(grid)[] = {
+                        {
+                            safeZoneX + safeZoneW - 4.2 * GUI_GRID_W,
+                            safeZoneY + safeZoneH - 2.2 * GUI_GRID_H,
+                            4 * GUI_GRID_W,
+                            2 * GUI_GRID_H
+                        },
+                        GUI_GRID_W,
+                        GUI_GRID_H
+                    };
                 };
             };
         };
         class Variables {
-            class grid_ACEX_FieldRations {
-                displayName = COMPONENT_NAME;
+            class GVAR(grid) {
+                displayName = CSTRING(IGUI_DisplayName);
                 description = CSTRING(IGUI_Description);
                 preview = QPATHTOF(ui\igui_preview.paa);
                 saveToProfile[] = {0, 1};
