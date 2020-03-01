728d797b15cf194001bef97397fd14261d031edd
jonpas <jonpas33@gmail.com>
2020-03-01 01:47:14 +0100

    Include required files and cleanup main component

--- addons/fortify/script_component.hpp
+++ addons/fortify/script_component.hpp
@@ -14,7 +14,7 @@
 #endif
 
 #include "\z\acex\addons\main\script_macros.hpp"
-#include "\a3\editor_f\Data\Scripts\dikCodes.h"
+#include "\a3\ui_f\hpp\defineDIKCodes.inc"
 
 #define PLACE_WAITING -1
 #define PLACE_CANCEL 0
