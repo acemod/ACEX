6c46abebf099fc417f237d6eee879fd95107cf83
mharis001 <34453221+mharis001@users.noreply.github.com>
2020-02-29 20:05:14 -0500

    Fortify - Add green presets to setup module (#217)

--- addons/fortify/CfgVehicles.hpp
+++ addons/fortify/CfgVehicles.hpp
@@ -28,19 +28,54 @@ class CfgVehicles {
                 displayName = "$STR_eval_typeside";
                 typeName = "NUMBER";
                 class values {
-                    class West { name = "$STR_WEST"; value = 1; default = 1; };
-                    class East { name = "$STR_east"; value = 2; };
-                    class Indp { name = "$STR_guerrila"; value = 3; };
-                    class Civ { name = "$STR_civilian"; value = 4; };
+                    class West {
+                        name = "$STR_WEST";
+                        value = 1;
+                        default = 1;
+                    };
+                    class East {
+                        name = "$STR_east";
+                        value = 2;
+                    };
+                    class Indp {
+                        name = "$STR_guerrila";
+                        value = 3;
+                    };
+                    class Civ {
+                        name = "$STR_civilian";
+                        value = 4;
+                    };
                 };
             };
             class Preset {
                 displayName = "$STR_controls_presets";
                 typeName = "NUMBER";
                 class values {
-                    class small { name = "$STR_small"; value = 1; default = 1; };
-                    class medium { name = "$STR_controller_sensitivity_medium"; value = 2; };
-                    class big { name = "$STR_large"; value = 3; };
+                    class small {
+                        name = "$STR_small";
+                        value = 1;
+                        default = 1;
+                    };
+                    class medium {
+                        name = "$STR_controller_sensitivity_medium";
+                        value = 2;
+                    };
+                    class big {
+                        name = "$STR_large";
+                        value = 3;
+                    };
+                    class smallGreen {
+                        name = CSTRING(SmallGreen);
+                        value = 4;
+                    };
+                    class mediumGreen {
+                        name = CSTRING(MediumGreen);
+                        value = 5;
+                    };
+                    class bigGreen {
+                        name = CSTRING(LargeGreen);
+                        value = 6;
+                    };
                 };
             };
             class Budget {
@@ -65,7 +100,7 @@ class CfgVehicles {
         canSetArea = 1;
         function = QFUNC(buildLocationModule);
         class AttributeValues {
-            size3[] = {300,300,-1};
+            size3[] = {300, 300, -1};
             IsRectangle = 1;
         };
     };
