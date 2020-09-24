6c34126a91b3c8bfab7781d6bf489172891bdeb7
Andreas Brostrom <andreas.brostrom.ce@gmail.com>
2020-09-24 19:41:34 +0200

    Added items

---
+++ addons/items/script_component.hpp
@@ -0,0 +1,17 @@
+#define COMPONENT items
+#define COMPONENT_BEAUTIFIED Items
+#include "\z\acex\addons\main\script_mod.hpp"
+
+// #define DEBUG_MODE_FULL
+// #define DISABLE_COMPILE_CACHE
+// #define ENABLE_PERFORMANCE_COUNTERS
+
+#ifdef DEBUG_ENABLED_items
+    #define DEBUG_MODE_FULL
+#endif
+
+#ifdef DEBUG_SETTINGS_items
+    #define DEBUG_SETTINGS DEBUG_SETTINGS_items
+#endif
+
+#include "\z\acex\addons\main\script_macros.hpp"
